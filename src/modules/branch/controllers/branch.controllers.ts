810a077a06c6d7ff87d156a535883c5ffc31ba98
bagasargita <argitabagasmahasmara@gmail.com>
2025-05-07 15:47:45 +0700

    #microservice_branch init

--- src/modules/branch/controllers/branch.controllers.ts
+++ src/modules/branch/controllers/branch.controllers.ts
@@ -1,22 +1,7 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Serialize } from 'src/decorators/serialize.decorator';
-import {
-  CreateBranchDto,
-  BranchResponseDto,
-  UpdateBranchDto,
-  QueryBranchDto,
-} from '../dtos/branch.dtos';
-import { GenericResponseDto } from 'src/dtos/generic.response.dto';
+import { BranchResponseDto, QueryBranchDto } from '../dtos/branch.dtos';
 import { BranchService } from '../services/branch.services';
 import { BranchSchedulerService } from '../scheduler/branch.scheduler';
 import { MetaBranchDtoByDate } from '../dtos/meta-branch.dtos';
@@ -51,34 +36,34 @@ export class BranchController {
     return this.branchService.getBranches(query);
   }
 
-  @ApiBearerAuth('accessToken')
-  @Get(':id')
-  @Serialize(BranchResponseDto)
-  getBranchesById(@Param('id') id: string): Promise<BranchResponseDto> {
-    return this.branchService.getBranchesById(Number(id));
-  }
+  // @ApiBearerAuth('accessToken')
+  // @Get(':id')
+  // @Serialize(BranchResponseDto)
+  // getBranchesById(@Param('id') id: string): Promise<BranchResponseDto> {
+  //   return this.branchService.getBranchesById(Number(id));
+  // }
 
-  @ApiBearerAuth('accessToken')
-  @Put(':id')
-  @Serialize(BranchResponseDto)
-  updateBranch(
-    @Param('id') id: string,
-    @Body() data: UpdateBranchDto,
-  ): Promise<BranchResponseDto> {
-    return this.branchService.updateBranch(Number(id), data);
-  }
+  // @ApiBearerAuth('accessToken')
+  // @Put(':id')
+  // @Serialize(BranchResponseDto)
+  // updateBranch(
+  //   @Param('id') id: string,
+  //   @Body() data: UpdateBranchDto,
+  // ): Promise<BranchResponseDto> {
+  //   return this.branchService.updateBranch(Number(id), data);
+  // }
 
-  @ApiBearerAuth('accessToken')
-  @Delete(':id')
-  @Serialize(GenericResponseDto)
-  deleteBranch(@Param('id') id: string): Promise<GenericResponseDto> {
-    return this.branchService.deleteBranches(Number(id));
-  }
+  // @ApiBearerAuth('accessToken')
+  // @Delete(':id')
+  // @Serialize(GenericResponseDto)
+  // deleteBranch(@Param('id') id: string): Promise<GenericResponseDto> {
+  //   return this.branchService.deleteBranches(Number(id));
+  // }
 
-  @ApiBearerAuth('accessToken')
-  @Post()
-  @Serialize(BranchResponseDto)
-  createBranch(@Body() data: CreateBranchDto): Promise<BranchResponseDto> {
-    return this.branchService.createBranch(data);
-  }
+  // @ApiBearerAuth('accessToken')
+  // @Post()
+  // @Serialize(BranchResponseDto)
+  // createBranch(@Body() data: CreateBranchDto): Promise<BranchResponseDto> {
+  //   return this.branchService.createBranch(data);
+  // }
 }
